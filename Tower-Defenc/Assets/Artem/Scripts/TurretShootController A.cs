96fe553514dff84f5468c05dbb28d7c8291650e7
artemZhubrev <artem.zhubrev@gmail.com>
2024-04-16 19:32:00 +0200

    commit

--- Tower-Defenc/Assets/Artem/Scripts/TurretShootController A.cs
+++ Tower-Defenc/Assets/Artem/Scripts/TurretShootController A.cs
@@ -1,21 +1,56 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 public class TurretShootControllerA : MonoBehaviour
 {
-    [SerializeField] private Transform turretGun;
-    private void Start()
-    {
-        
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("Enemy"))
-        {
+    [SerializeField] private GameObject prefabBullet;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float cooldown;
 
-            turretGun.localRotation = Quaternion.LookRotation(other.transform.position, other.transform.position);
+    private GameObject _target;
+
+    IEnumerator OnFire()
+    {
+        while (prefabBullet != null)
+        {
+            GameObject getBullet = Instantiate(prefabBullet, firePoint.position, Quaternion.identity);
+            firePoint.transform.LookAt(_target.transform.position);
+
+            Rigidbody rb = getBullet.GetComponent<Rigidbody>();
+            rb.AddForce(firePoint.forward * 100f, ForceMode.Impulse);
+
+            yield return new WaitForSeconds(cooldown);
         }
     }
+
+    #region GetTarget
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") &&
+            _target == null)
+        {
+            _target = other.gameObject;
+            StartCoroutine(OnFire());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && _target != null)
+        {
+            _target = null;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy") && _target == null)
+        {
+            _target = other.gameObject;
+            StartCoroutine(OnFire());
+        }
+    }
+    #endregion
+
 }
