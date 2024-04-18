58609f27173e107f23099fb91f9215d19aa7a400
artemZhubrev <artem.zhubrev@gmail.com>
2024-04-18 17:13:17 +0200

    commit

--- Tower-Defenc/Assets/Artem/Scripts/TurretShootController A.cs
+++ Tower-Defenc/Assets/Artem/Scripts/TurretShootController A.cs
@@ -1,14 +1,21 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TurretShootControllerA : MonoBehaviour
 {
     [SerializeField] private GameObject prefabBullet;
     [SerializeField] private Transform firePoint;
+    [SerializeField] private SphereCollider collider;
     [SerializeField] private float cooldown;
+    [SerializeField] private float range;
 
     private GameObject _target;
 
+    private void Start()
+    {
+        collider.radius = range;
+    }
     IEnumerator OnFire()
     {
         while (prefabBullet != null)
