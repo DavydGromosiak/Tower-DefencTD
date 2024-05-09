2ae947b7d66e330c279127cab5a85ff3160e0f74
artemZhubrev <artem.zhubrev@gmail.com>
2024-05-09 17:42:34 +0200

    commit

--- Tower-Defenc/Assets/Davyd/_Coding/Menu/LoadLevel.cs
+++ Tower-Defenc/Assets/Davyd/_Coding/Menu/LoadLevel.cs
@@ -6,20 +6,24 @@ public class LoadLevel : MonoBehaviour
     public void Level01()
     {
         SceneManager.LoadScene("Level1");
+        Time.timeScale = 1f;
     }
     
     public void Level02()
     {
         SceneManager.LoadScene("Level2");
+        Time.timeScale = 1f;
     }
     
     public void Level03()
     {
         SceneManager.LoadScene("Level3");
+        Time.timeScale = 1f;
     }
     
     public void BackMenu()
     {
         SceneManager.LoadScene("_MainMenu");
+        Time.timeScale = 1f;
     }
 }
